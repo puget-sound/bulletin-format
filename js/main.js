be69dbdb8539e038209de09cc9fe09b218ffedf9
Annie Cain <anniejocain@gmail.com>
2021-04-30 15:03:41 -0700

    added members and course details

--- js/main.js
+++ js/main.js
@@ -3,14 +3,31 @@ var searchParams = new URLSearchParams(window.location.search);
 var department = searchParams.get("department");
 var year = searchParams.get("year");
 
-fetch(bulletinApiUrl + department + "&acad_year=")
+fetch(bulletinApiUrl + department + "&acad_year=" + year)
   .then(function(response) {
     return response.json();
   })
   .then(function(json) {
     var department_source = document.getElementById("department-template").innerHTML;
     var department_template = Handlebars.compile(department_source);
-    var department_info = json.results.acad_org;
+    Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    });
+    Handlebars.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+});
+    var department_info = json.results;
     var department_html = department_template(department_info);
     document.getElementById('department').innerHTML = department_html;
   });
